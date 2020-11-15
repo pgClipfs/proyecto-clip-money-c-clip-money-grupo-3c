309da7b93e672ada56ba62433a958b8571476093
lautaro garcia <lautarojgarcia177@gmail.com>
2020-11-14 21:31:13 -0300

    Se crea el proyecto .NET MVC

---
+++ Back end/ClipMoneyGrupo3C/ClipMoneyGrupo3C/Controllers/PasswordsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClipMoneyGrupo3C.Models;
+
+namespace ClipMoneyGrupo3C.Controllers
+{
+    public class PasswordsController : Controller
+    {
+        private clipMoneyEntities db = new clipMoneyEntities();
+
+        // GET: Passwords
+        public ActionResult Index()
+        {
+            var passwords = db.passwords.Include(p => p.usuario);
+            return View(passwords.ToList());
+        }
+
+        // GET: Passwords/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            password password = db.passwords.Find(id);
+            if (password == null)
+            {
+                return HttpNotFound();
+            }
+            return View(password);
+        }
+
+        // GET: Passwords/Create
+        public ActionResult Create()
+        {
+            ViewBag.usuarioId = new SelectList(db.usuarios, "dni", "nombre");
+            return View();
+        }
+
+        // POST: Passwords/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,usuarioId,valor,fecha")] password password)
+        {
+            if (ModelState.IsValid)
+            {
+                db.passwords.Add(password);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.usuarioId = new SelectList(db.usuarios, "dni", "nombre", password.usuarioId);
+            return View(password);
+        }
+
+        // GET: Passwords/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            password password = db.passwords.Find(id);
+            if (password == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.usuarioId = new SelectList(db.usuarios, "dni", "nombre", password.usuarioId);
+            return View(password);
+        }
+
+        // POST: Passwords/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,usuarioId,valor,fecha")] password password)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(password).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.usuarioId = new SelectList(db.usuarios, "dni", "nombre", password.usuarioId);
+            return View(password);
+        }
+
+        // GET: Passwords/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            password password = db.passwords.Find(id);
+            if (password == null)
+            {
+                return HttpNotFound();
+            }
+            return View(password);
+        }
+
+        // POST: Passwords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            password password = db.passwords.Find(id);
+            db.passwords.Remove(password);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Back end/ClipMoneyGrupo3C/ClipMoneyGrupo3C/Controllers/UsuariosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClipMoneyGrupo3C.Models;
+
+namespace ClipMoneyGrupo3C.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private clipMoneyEntities db = new clipMoneyEntities();
+
+        // GET: usuarios
+        public ActionResult Index()
+        {
+            var usuarios = db.usuarios.Include(u => u.datosUsuario);
+            return View(usuarios.ToList());
+        }
+
+        // GET: usuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.dni = new SelectList(db.datosUsuarios, "usuarioId", "domicilio");
+            return View();
+        }
+
+        // POST: usuarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "dni,nombre,apellido,email,frente,dorso,estado")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuarios.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.dni = new SelectList(db.datosUsuarios, "usuarioId", "domicilio", usuario.dni);
+            return View(usuario);
+        }
+
+        // GET: usuarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.dni = new SelectList(db.datosUsuarios, "usuarioId", "domicilio", usuario.dni);
+            return View(usuario);
+        }
+
+        // POST: usuarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "dni,nombre,apellido,email,frente,dorso,estado")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.dni = new SelectList(db.datosUsuarios, "usuarioId", "domicilio", usuario.dni);
+            return View(usuario);
+        }
+
+        // GET: usuarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            usuario usuario = db.usuarios.Find(id);
+            db.usuarios.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
