6b7ffb6bc9c3c8954da7c52b1ca4aa83f58bf471
lautaro garcia <lautarojgarcia177@gmail.com>
2020-11-24 21:12:24 -0300

    email settings

--- ClipMoney3C/ClipMoney3C/Services/EmailSender.cs
+++ ClipMoney3C/ClipMoney3C/Services/EmailSender.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
@@ -11,25 +12,23 @@ namespace ClipMoney3C.Services
 {
     public class EmailSender : IEmailSender
     {
-        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        private readonly IConfiguration Configuration;
+        public EmailSender(IConfiguration configuration)
         {
-            Options = optionsAccessor.Value;
+            Configuration = configuration;
         }
-
-        public AuthMessageSenderOptions Options { get; }
 
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            return Execute(Options.SendGridKey, subject, message, email);
+            return Execute(Configuration["SendGrid:Key"], subject, message, email);
         }
 
         public Task Execute(string apiKey, string subject, string message, string email)
         {
-            //var apiKey = "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";
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
-                From = new EmailAddress("lautaroxg10@gmail.com", Options.SendGridUser),
+                From = new EmailAddress(Configuration["SendGrid:Sender"], Configuration["SendGrid:User"]),
                 Subject = subject,
                 PlainTextContent = message,
                 HtmlContent = message
